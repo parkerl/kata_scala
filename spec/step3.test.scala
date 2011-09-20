fc93b1c52dff4bf46de56871a70f93bb4a130f60
parkerl <parker@earthvectors.com>
2011-09-20 16:25:34 -0600

    Initial Commit This repo will contain the 15 steps I use to learn a new programming language. In this case Scala.

---
+++ spec/step3.test.scala
@@ -0,0 +1,17 @@
+import org.scalatest.Spec
+import org.scalatest.matchers.ShouldMatchers
+import scala.collection.mutable.Stack
+import learning.FibPrinter
+import spec.MockOutput
+
+class NumberCruncherSpec extends Spec with ShouldMatchers {
+
+  describe("Take a series of numbers and sort them in either ascending or descending order") {
+      
+      it("should print 123 when given 1,2,3 as input") {
+         
+      }
+      
+      
+  }
+}
